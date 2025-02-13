6cf27037e3d5b0c674347c53629c6bf14d06d840
Seung(emily_the_dev) <emily101304@naver.com(emily101304)>
2025-02-13 21:13:03 +0900

    BaseEntitiy created

---
+++ src/main/java/com/example/schedule_jpa_final/ScheduleJpaFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule_jpa_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJpaFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleJpaFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedule_jpa_final/ScheduleJpaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedule_jpa_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleJpaFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
